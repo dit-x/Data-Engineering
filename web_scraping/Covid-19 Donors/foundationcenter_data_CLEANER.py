733a95fa38813c84dd84ab6e1281306a20eecd56
Timo-ffex <iffexibility.id@gmail.com>
2020-06-18 16:38:14 +0100

    cleaned the extracted data and uploaded all other files associated

---
+++ web_scraping/Covid-19 Donors/foundationcenter_data_CLEANER.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import re
+
+df = pd.read_csv(r"C:\DiT\Programming\Git\Data-Engineering\web_scraping\Covid-19 Donors\covid19_Donors.csv")
+
+def convert(x):
+    if "B" in x:
+        return round(float(x.replace("Billion",""))*1000000000, 2)
+    elif  "M" in x:
+        return round(float(x.replace("Million",""))*1000000, 2)
+    else:
+        return round(float(x), 2)
+
+df["value_granted"] = df.value_granted.apply(lambda x: x.replace("$", "").replace(",",""))
+
+df["value_granted"] = df["value_granted"].apply(convert)
+
+df.to_csv("covid19_donors_data_cleaned.csv")
