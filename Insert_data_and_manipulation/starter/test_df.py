b49b3ee45029599dcffd838dec7cdd06105f3441
Timo-ffex <iffexibility.id@gmail.com>
2021-01-08 03:10:55 +0100

    file modified

---
+++ Insert_data_and_manipulation/starter/test_df.py
@@ -0,0 +1,29 @@
+#!usr/python3.8
+import unittest
+import pandas as pd
+from pandas.util.testing import assert_frame_equal # <-- for testing dataframes
+
+class DFTests(unittest.TestCase):
+
+    """ class for running unittests """
+
+    def get_test_file(self):
+        """ Your setUp """
+        TEST_INPUT_DIR = 'data/'
+        test_file_name =  'customers.csv'
+        try:
+            data = pd.read_csv(test_file_name,
+                sep = ',',
+                header = 0)
+        except IOError:
+            print('cannot open file')
+        self.fixture = data
+
+    def test_dataFrame_constructedAsExpected(self):
+        """ Test that the dataframe read in equals what you expect"""
+        processed_df = pd.read_csv('cus.csv')
+        assert_frame_equal(self.fixture, processed_df)
+
+
+if __name__ == "__main__":
+    unittest.main()
